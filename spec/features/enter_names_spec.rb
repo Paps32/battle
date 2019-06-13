cf51fe9d704f46729a10479b35cf9f48603218a2
Student <student@MAKERSs-MacBook-Pro.local>
2019-06-13 15:18:03 +0100

    entering and storing player names

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,11 @@
+
+
+feature 'players can enter their names' do
+  scenario 'our form can accept names and display them' do
+    visit('/')
+    fill_in :player_1, with: 'Papya'
+    fill_in :player_2, with: 'Lucian'
+    click_button 'Submit'
+    expect(page).to have_content 'Papya vs Lucian'
+    end
+end
