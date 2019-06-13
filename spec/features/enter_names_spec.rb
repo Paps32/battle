216956b97376ca40c049dcdb728cf75d4c2d3829
Student <student@MAKERSs-MacBook-Pro.local>
2019-06-13 16:00:26 +0100

    enable sessions and redirect

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,8 +4,8 @@ feature 'players can enter their names' do
   scenario 'our form can accept names and display them' do
     visit('/')
     fill_in :player_1, with: 'Papya'
-    fill_in :player_2, with: 'Lucian'
+    fill_in :player_2, with: 'Dracula'
     click_button 'Submit'
-    expect(page).to have_content 'Papya vs Lucian'
+    expect(page).to have_content 'Papya vs Dracula'
     end
 end
